f9ac8f7fbc660e45491c5f8df35c53c401f0c19b
Luis Enrique Guzman Pineda <luis.guzp13@gmail.com>
2022-08-17 17:12:36 -0500

    app finished

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,2 +1,39 @@
 class RestaurantsController < ApplicationController
+  # before_action :set_restaurant, only: %i[show edit update destroy chef]
+  def index
+    @restaurants = Restaurant.all
+  end
+
+  def show
+    @restaurant = Restaurant.find(params[:id])
+  end
+
+  def new
+    @restaurant = Restaurant.new
+  end
+
+  def create
+    @restaurant = Restaurant.create(restaurant_params)
+    @restaurant.save
+    if @restaurant.save
+      redirect_to restaurant_path(@restaurant)
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def review
+    @review = Review.new
+    @review = @restaurant.review
+  end
+
+  private
+
+  def restaurant_params
+    params.require(:restaurant).permit(:name, :phone_number, :address, :category)
+  end
+
+  # def set_restaurant
+
+  # end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,4 @@
+require 'faker'
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
 #
@@ -5,3 +6,15 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+category = ["chinese", "italian", "japanese", "french", "belgian"]
+number = 10.times.map { rand(10) }.join.to_i
+# ratings = rand(5)
+2.times do
+  restaurant = Restaurant.create(name: Faker::Restaurant.name, address: Faker::Address.street_name, category: category.sample, phone_number: number )
+  3.times do
+    review = Review.new(content: Faker::Restaurant.description, rating: rand(5))
+    review.restaurant = restaurant
+    review.save
+  end
+end
